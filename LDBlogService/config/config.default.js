de9794ebf674f6e94b5fc23ffe02c7233b556136
forever-Liudawang <2387498813@qq.com>
2021-11-01 10:32:24 +0800

    server.js

--- LDBlogService/config/config.default.js
+++ LDBlogService/config/config.default.js
@@ -45,6 +45,13 @@ module.exports = appInfo => {
       },
     },
   };
+  config.cluster = {
+    listen: {
+      path: '',
+      port: 8000,
+      hostname: '0.0.0.0',
+    }
+  };
   // add your user config here
   const userConfig = {
     // myAppName: 'egg',

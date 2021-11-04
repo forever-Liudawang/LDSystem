4190e1045916480bc38cc6f384fe0af9e290bf42
forever-Liudawang <2387498813@qq.com>
2021-11-04 14:46:25 +0800

    merge

--- LDBlogWeb/src/views/articleList/index.vue
+++ LDBlogWeb/src/views/articleList/index.vue
@@ -35,14 +35,14 @@
         <el-table-column prop="userName" label="创建用户" width="100" />
         <el-table-column prop="articleCate" label="文章分类" :formatter="cateFormat" width="160" />
         <el-table-column prop="articleTitle" label="文章标题" />
-        <el-table-column prop="isMyRecommend" label="文章热推" >
+        <el-table-column prop="isMyRecommend" label="文章热推">
           <template slot-scope="scope">
             <el-switch
-              @change="handleSwitch(scope)"
               v-model="scope.row.isMyRecommend"
               active-color="#13ce66"
-              inactive-color="#ff4949">
-            </el-switch>
+              inactive-color="#ff4949"
+              @change="handleSwitch(scope)"
+            />
           </template>
         </el-table-column>
         <el-table-column label="操作" width="180">
@@ -88,7 +88,7 @@ export default {
         searchKey: '',
         isMyRecommend: false
       },
-      curRecommend:false
+      curRecommend: false
     }
   },
   mounted() {
@@ -162,10 +162,10 @@ export default {
       console.log(`data`, data)
       const resp = await this.$http({
         url: '/article/switchRecommendArticle',
-        data: { articleId:data._id,newState:data.isMyRecommend },
+        data: { articleId: data._id, newState: data.isMyRecommend },
         method: 'post'
       })
-      if(resp && resp.success){
+      if (resp && resp.success) {
         this.getDataList()
       }
     }

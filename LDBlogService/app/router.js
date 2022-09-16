9b8a665d5b670418c73c65775fbe3b611da4eb47
andyLiu <lmy15973914530@163.com>
2022-09-16 11:15:42 +0800

    23

--- LDBlogService/app/router.js
+++ LDBlogService/app/router.js
@@ -52,4 +52,44 @@ module.exports = app => {
   router.post('/test/add', controller.test.add)
   router.get('/test/getList', controller.test.getList)
   router.get('/test/getMenu', controller.test.getMenu)
+
+
+  router.post('/user/insert', controller.user.insert);
+  router.get('/user/getUser', controller.user.getUser);
+  router.get('/user/userNameIsRepeat', controller.user.userNameIsRepeat);
+  router.post('/user/login', controller.user.login);
+  router.post('/user/updateAvatar', controller.user.updateAvatar);
+  router.post('/user/updateUserName', controller.user.updateUserName);
+  router.post('/user/updateSign', controller.user.updateSign);
+  router.post('/user/updatePsd', controller.user.updatePsd);
+  router.get('/user/getUserById', controller.user.getUserById);
+  
+  router.post('/post/uploadFile', controller.post.uploadFile);
+  router.post('/post/uploadImg', controller.post.uploadImg);
+  router.post('/post/insert', controller.post.insert);
+  router.get('/post/getPost', controller.post.getList);
+  router.post('/post/light', controller.post.light);
+  router.post('/post/unLight', controller.post.unLight);
+  router.get('/post/getPostById', controller.post.getPostById);
+  router.get('/post/getMyPost', controller.post.getMyPost);
+  router.get("/post/getRecommend",controller.post.getRecommend)
+  
+  router.get("/music/getBannerList",controller.music.getBannerList)
+  router.get("/music/getRecommendList",controller.music.getRecommendList)
+  router.get("/music/getRecommendMusicList",controller.music.getRecommendMusicList)
+  router.get("/music/getHotSinger",controller.music.getHotSinger)
+  router.get("/music/getSingerList",controller.music.getSingerList)
+  router.get("/music/getSingerDetail",controller.music.getSingerDetail)
+  router.post("/music/likeMusic",controller.music.likeMusic)
+  router.post("/music/isLiked",controller.music.isLiked)
+  
+  
+  router.get("/follow/getFollow",controller.follow.getFollow)
+  router.post("/follow/following",controller.follow.following)
+  router.post("/follow/isFollowed",controller.follow.isFollowed)
+  router.post("/follow/unFollowing",controller.follow.unFollowing)
+
+  router.post("/comment/insert",controller.comment.insert)
+  router.get("/comment/getComment",controller.comment.getComment)
+  router.get("/comment/getReplayComment",controller.comment.getReplayComment)
 };
